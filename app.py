819cf120c07da6a78a415310cbc93086050b7baf
dlatldydid18@naver.com <dlatldydid18@naver.com>
2022-10-12 18:34:11 +0900

    heroku

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask
 
-def create_app():
-  app = Flask(__name__)
-  app.jinja_env.add_extension('jinja2.ext.loopcontrols')
-  import pages
-  app.register_blueprint(pages.bp)
-  return app
+app = Flask(__name__)
+app.jinja_env.add_extension('jinja2.ext.loopcontrols')
+import pages
+app.register_blueprint(pages.bp)
+app.run()
